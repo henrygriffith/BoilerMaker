98a1b602c0b3d4ca89e439d307446f287dc018e6
Henry Griffith <hgriffithdev@gmail.com>
2019-10-29 17:50:58 -0500

    5 && 7. Sequelize / reviewed environment variables and synced database

--- server/server.js
+++ server/server.js
@@ -2,12 +2,6 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const bodyParser = require('bodyParser')
-const port = process.env.PORT || 3000; // this can be very useful if you deploy to Heroku!
-app.listen(port, function () {
-  console.log("Knock, knock");
-  console.log("Who's there?");
-  console.log(`Your server, listening on port ${port}`);
-});
 
 //-----------logging middleware-------------//
 app.use(morgan('dev'))
---
+++ server/start.js
@@ -0,0 +1,14 @@
+//our sequelize instance is created in db.js
+const db = require('./db.js')
+//our server we created is at previous entry point server.js
+const app = require('./server');
+const port = process.env.PORT || 3000;
+
+db.sync()
+  .then(function(){
+      app.listen(port, function () {
+        console.log("Knock, knock");
+        console.log("Who's there?");
+        console.log(`Your server, listening on port ${port}`);
+      })
+  })
